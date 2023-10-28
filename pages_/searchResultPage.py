4987416a3c09728d36e9fbaf726e15f637d96e91
NariNen <narinegrigoryane@gmail.com>
2023-10-28 15:00:22 +0400

    Search field functionality testing

--- pages_/searchResultPage.py
+++ pages_/searchResultPage.py
@@ -4,5 +4,5 @@ from selenium.webdriver.common.by import By
 class SearchResultPage(BasePage):
 
     def click_to_first_product(self):
-        firstProductElement = self._find_element(By.XPATH, "(//a[@target='_blank]')[2]")
+        firstProductElement = self._find_element(By.XPATH, "(//a[@target='_blank'])[2]")
         self._click(firstProductElement)
